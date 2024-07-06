48d3bbbe2ead91ea67344883c1e035dbf062b9fd
junggeun <thethe133@naver.com>
2024-07-06 14:13:57 +0900

    Initial commit

---
+++ src/test/java/com/example/tddorderservice/TddOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tddorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
