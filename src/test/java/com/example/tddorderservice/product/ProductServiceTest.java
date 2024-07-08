fbc62d247db456e812c9e3d916ef2de66b4c4d44
junggeun <thethe133@naver.com>
2024-07-08 18:59:46 +0900

    feat: Spring Boot 상품 등록 기능

--- src/test/java/com/example/tddorderservice/product/ProductServiceTest.java
+++ src/test/java/com/example/tddorderservice/product/ProductServiceTest.java
@@ -2,19 +2,15 @@ package com.example.tddorderservice.product;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class ProductServiceTest {
 
+    @Autowired
     private ProductService productService;
-    private ProductPort productPort;
-    private ProductRepository productRepository;
 
-    @BeforeEach
-    void setUp() {
-        productRepository = new ProductRepository();
-        productPort = new ProductAdaptor(productRepository);
-        productService = new ProductService(productPort);
-    }
 
     @Test
     void 상품등록(){
