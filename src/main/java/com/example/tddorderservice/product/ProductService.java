a3055cc0df0fbbb7bbec45705e60959845af29ea
junggeun <thethe133@naver.com>
2024-07-19 18:05:58 +0900

    feat: POJO 상품 수정 기능

--- src/main/java/com/example/tddorderservice/product/ProductService.java
+++ src/main/java/com/example/tddorderservice/product/ProductService.java
@@ -40,6 +40,9 @@ class ProductService {
 
     public void updateProduct(final Long productId, final UpdateProductRequest request) {
         final Product product = productPort.getProduct(productId);
+
         product.update(request.name(), request.price(), request.discountPolicy());
+
+        productPort.save(product);
     }
 }
